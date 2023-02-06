48a195462a31b09a43f3b30ef48a6775d9eaa00c
TareqHaider93 <110522340+TareqHaider93@users.noreply.github.com>
2023-02-06 13:20:13 +0600

    syncfusion demo working without style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ScheduleAllModule, RecurrenceEditorAllModule } from '@syncfusion/ej2-angular-schedule';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ChartAllModule, AccumulationChartAllModule, RangeNavigatorAllModule } from '@syncfusion/ej2-angular-charts';
+
+import { GridAllModule } from '@syncfusion/ej2-angular-grids';
+
+import { TabAllModule } from '@syncfusion/ej2-angular-navigations';
+
+import { DatePickerModule } from '@syncfusion/ej2-angular-calendars';
+
+import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
+
+import { RadioButtonModule } from '@syncfusion/ej2-angular-buttons';
+
+import { CheckBoxModule , ButtonModule } from '@syncfusion/ej2-angular-buttons';
+
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule({ declarations: [ AppComponent ], imports: [ RadioButtonModule, CheckBoxModule, BrowserModule, ButtonModule, DialogModule, RadioButtonModule, ChartAllModule, RangeNavigatorAllModule, AccumulationChartAllModule, GridAllModule, RichTextEditorAllModule, ScheduleAllModule, RecurrenceEditorAllModule, CheckBoxModule, FormsModule, ReactiveFormsModule, CommonModule, BrowserModule, TabAllModule, DatePickerModule], providers: [], bootstrap: [AppComponent]
 })
 export class AppModule { }
